c39c8f76e38c217115352f9bdbd104104c7be79e
JKRyan <jed@jedkendall.com>
2019-02-23 09:30:27 -0600

    All done! Any other additions will be essentially just superficial or for flair.

--- assets/javascript/quizsubmit.js
+++ assets/javascript/quizsubmit.js
@@ -40,4 +40,41 @@ function displayPlayerScore(scorePercent, correctArray) {
 
     console.log("Player Score: " + scorePercent + "% Correct");
     $(".form-check-input").prop("disabled", true);
+
+    var newDiv = $("<div>");
+    var newScoreDisplay = $("<h3>");
+
+    newDiv.addClass("col-12 d-flex flex-column justify-content-center");
+    newScoreDisplay.attr("id", "score-display-number");
+    newDiv.html("<h2>Thine Total Score Be:</h2><br>");
+    newScoreDisplay.html(scorePercent + "%");
+    newDiv.append(newScoreDisplay);
+
+    $("#results-display-div").append(newDiv);
+
+    var answersList = $("<div>");
+
+    answersList.addClass("col-12 offset-0 col-md-6 offset-md-3");
+    answersList.attr("id", "answers-list");
+    for (var i = 0; i < correctArray.length; i++) {
+        var newPara = $("<p>");
+        newPara.addClass("answer-para");
+        if (correctArray[i]) {
+            newPara.html(i+1 + ". Correct.");
+        }
+
+        else if (!correctArray[i]) {
+            newPara.html(i+1 + ". Incorrect. Answer: " + triviaObjects[i].correct_answer);
+        }
+
+        answersList.append(newPara);
+    }
+
+    $("#results-display-div").append(answersList);
+    if (scorePercent <= 1) {
+        var newPara = $("<p>");
+        newPara.text("Not a single one correct at all? Really? Did you even answer any of them? Or did you just scroll down to the bottom to press Submit and test what would happen? I really hope it's the latter. Unbelievable.")
+        answersList.append(newPara);
+    }
+
 }
